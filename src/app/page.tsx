ccafb764c1fa1c02f04b98a67a21907d0ff4b591
Ajmalniz <128520849+Ajmalniz@users.noreply.github.com>
2023-09-10 13:43:21 +0500

    side menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,32 @@
-"use client";
-
+"use client"
 import { CldImage } from "next-cloudinary";
 import { CldUploadButton } from "next-cloudinary";
 import { useState } from "react";
 
-export type UploadResult = {
-  info: {
-    public_id: string;
+type UploadResult ={
+  info :{
+    public_id:string
   };
-  event: "success";
+  event:'success';
 };
-
 export default function Home() {
   const [imageId, setImageId] = useState("");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CldUploadButton
-        onClick={(result: UploadResult) => {
+        onUpload={(result :UploadResult) => {
           setImageId(result.info.public_id);
         }}
-        uploadPreset="giomsr4s"
+        uploadPreset="wmzj2iws"
       />
 
       {imageId && (
         <CldImage
+          key={imageId}
           width="500"
           height="300"
           src={imageId}
-          sizes="100vw"
           alt="Description of my image"
         />
       )}
