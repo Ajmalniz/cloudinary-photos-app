66d6b386d9a3288d798a2c1fd25c242174ee422f
Ajmalniz <128520849+Ajmalniz@users.noreply.github.com>
2023-09-12 15:12:39 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -119,7 +119,7 @@ export default function RootLayout({
               height="50"
               alt="icon of this photo album app"
             />
-            WebDevCody Photos
+            Ajmal Khan Photos
             <div className="ml-auto flex items-center space-x-4">
               <Avatar>
                 <AvatarImage
